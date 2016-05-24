bc51e13b73f6ce2d461ae4dd6d5922b1183468a4
cvilla100@gmail.com <camillevilla@Camille-Villas-MacBook-Air.local>
2016-05-23 23:13:29 -0700

    Added notes key to Pomodoro class

--- pomodoro.py
+++ pomodoro.py
@@ -1,15 +1,10 @@
 #define pomodoro class
 class Pomodoro():
 	def __init__(self, **kwargs):
-		self.date = input('Date (YYYY-MM-DD): ') #eventually have this entered automatically! 
-		#eventually define function to convert 24H time to 12H time. blech! 
-		self.time = input('Time (e.g. 11:00 PM): ') #also automatic one day 
+		self.date = input('Date (YYYY-MM-DD): ') #eventually default to current date
+		self.time = input('Time (e.g. 11:00 PM): ') #eventually default to current time minus 25 minnutes
 		self.tags = input('Enter tags as a comma separated list (e.g. x,y,z): ')
-		#add notes section! if empty, omit - check 
+		self.notes = input('Describe briefly')
 
 	def __str__(self):
 		return '{},{},{}'.format(self.date,self.time,self.tags)
-
-	#def get_tags(self):
-	#	tags = input('Enter tags as a comma separated list (e.g. x,y,z)')
-	#	tags = tags.split(',')
