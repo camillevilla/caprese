52cfccba845fb0f874fe2492ac3eaf74a93e1a3f
Sean Lee <seanboramlee@gmail.com>
2016-12-09 10:48:40 -0800

    set up config files

---
+++ app/index.js
@@ -0,0 +1,10 @@
+const React = require('react');
+const ReactDOM = require('react-dom')
+
+class HelloWorld extends React.Component{
+  return (
+    <div>Hello World!</div>
+  )
+}
+
+ReactDOM.render(<Component />, document.getElementById('app'))
